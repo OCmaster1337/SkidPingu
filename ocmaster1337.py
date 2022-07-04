f88ca8594b9d32e29f0185234f84a9f13889480a
OCmaster1337 <108698600+OCmaster1337@users.noreply.github.com>
2022-07-05 00:57:14 +0300

    Add files via upload

---
+++ ocmaster1337.py
@@ -0,0 +1,12 @@
+import os 
+import time
+ 
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print('\033[31m' + 'skidpingu v1.2')
+
+print('\033[31m' + 'OCmaster1337 on github')
+os.system("ping 1.1.1.1")
+time.sleep(99999)
